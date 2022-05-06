aefb3a940ce04705b50068356e25a3842a1e922a
lkeix <ai.kanaria.ai@gmail.com>
2022-05-06 18:01:44 +0900

    add: echo, grapql

--- server/serve.go
+++ server/serve.go
@@ -28,3 +28,35 @@ func main() {
 func hello(ctx echo.Context) error {
 	return ctx.String(http.StatusOK, "hello")
 }
+
+/*
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/lkeix/graphql-sandbox/graph"
+	"github.com/lkeix/graphql-sandbox/graph/generated"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+*/
--- server/start.sh
+++ server/start.sh
@@ -1,2 +1,4 @@
 goose up
+go run github.com/99designs/gqlgen init
+CGO_ENABLED=0 go build -o /go/src/app/serve serve.go
 ./serve
