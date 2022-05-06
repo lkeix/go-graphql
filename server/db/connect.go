1535a2bf4473d0491b016efaa0d06db9b117f20f
lkeix <ai.kanaria.ai@gmail.com>
2022-05-06 22:18:34 +0900

    fix: set env variables, instead of const values

--- server/db/connect.go
+++ server/db/connect.go
@@ -3,18 +3,13 @@ package db
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-const (
-	POSTGRES_HOST     = "db"
-	POSTGRES_PORT     = "5432"
-	POSTGRES_USER     = "user"
-	POSTGRES_PASSWORD = "password"
-	POSTGRES_DBNAME   = "go-graphql-sandbox"
-)
+const ()
 
 var (
 	DB  *gorm.DB
@@ -24,11 +19,11 @@ var (
 func Connect() {
 	opt := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		POSTGRES_HOST,
-		POSTGRES_PORT,
-		POSTGRES_USER,
-		POSTGRES_PASSWORD,
-		POSTGRES_DBNAME,
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DBNAME"),
 	)
 	DB, err = gorm.Open(
 		"postgres",
